9122e4b9e5273b2f088c88bc4748c76ff4751581
Freddy <nubiaggnailed@foxmail.com>
2022-08-18 09:59:20 +0800

    完成PermissionX库的开发与测试

--- app/src/main/java/com/permissionx/app/MainActivity.kt
+++ app/src/main/java/com/permissionx/app/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.permissionx.app
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.permissionx.mylibrary.PermissionX
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
+    private val button: Button by lazy { findViewById<Button>(R.id.button) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button.setOnClickListener {
+            PermissionX.request(
+                this,
+                android.Manifest.permission.CALL_PHONE,
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                android.Manifest.permission.READ_CONTACTS,
+            ) { allGranted, deniedList ->
+                if (allGranted) {
+                    Toast.makeText(this, "All permissions are granted", Toast.LENGTH_SHORT).show()
+                    call()
+                } else {
+                    Toast.makeText(this, "you denied $deniedList", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun call() {
+        try {
+            val intent = Intent(Intent.ACTION_CALL)
+            intent.data = Uri.parse("tel:10086")
+            startActivity(intent)
+        } catch (e: SecurityException) {
+            e.printStackTrace()
+        }
     }
 }
